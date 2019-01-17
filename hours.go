c4220c97d763f96aa98d6e098c7dc135c85c0d4f
William Zhang <willzhang05@gmail.com>
2019-01-17 17:40:35 -0500

    add hours script

---
+++ hours.go
@@ -0,0 +1,46 @@
+// This script is used for totaling up the time I work, based on the format given in the example file hours.txt,
+// Input is given through stdin.
+package main
+import (
+    "os"
+    "fmt"
+    "strings"
+    "bufio"
+    "time"
+)
+
+func main() {
+    scanner := bufio.NewScanner(os.Stdin)
+    var total time.Duration = 0
+    var subtotal time.Duration = 0
+    for scanner.Scan() {
+        line := scanner.Text()
+        line = strings.TrimSpace(line)
+        if (strings.ContainsAny(line, "/")) {
+            //const dayForm = "1/2"
+            //t, _ := time.Parse(dayForm, line)
+            if (subtotal.Minutes() != 0) {
+                fmt.Println(subtotal.Minutes())
+                fmt.Println("\n")
+            }
+            fmt.Println(line)
+            fmt.Println("---")
+            subtotal = 0
+        } else {
+            tp := strings.Split(line, "-")
+            const hmForm = "3:04pm"
+            t1, _ := time.Parse(hmForm, tp[0])
+            t2, _ := time.Parse(hmForm, tp[1])
+            diff := t2.Sub(t1)
+            if (!t2.After(t1)) {
+                offset, _ := time.ParseDuration("24h")
+                diff += offset
+            }
+            //fmt.Println(diff.Minutes())
+            subtotal += diff
+            total += diff
+        }
+    }
+    fmt.Println("Total:")
+    fmt.Println(total.Minutes())
+}
