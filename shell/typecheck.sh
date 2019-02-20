e331ba66fbefc4f0b16f2a051f8f3af883a1ba51
William Zhang <willzhang05@gmail.com>
2019-02-20 00:08:48 -0500

    add updated typecheck script

--- shell/typecheck.sh
+++ shell/typecheck.sh
@@ -1,38 +1,31 @@
 #!/bin/bash
+## This script is used to automatically typecheck student-submitted lean files.
+## It takes a path to the student subdirectories.
 
 if [ $# -ne 1 ]; then
     echo "Please provide a path to student submissions!"
     exit;
 fi
 
-#readarray -td '' array < <(find "$1" -maxdepth 1 -mindepth 1 -type d -print0)
-
-#array=()
-#while IFS= read -r -d $'\0'; do
-#    array+=("$SUBDIR")
-#    echo $SUBDIR
-#done < <(find "$1" -maxdepth 1 -mindepth 1 -type d -print0)
-
-#SUBMISSIONS=$(find "$1" -maxdepth 1 -mindepth 1 -type d -printf '"%f"\n')
-#IFS='\n' read -r -a array <<< $(find "$1" -maxdepth 1 -mindepth 1 -type d -printf '"%f"\n')
 START_DIR=$1
 
-for STUDENT in "$START_DIR"/*/; do
+for STUDENT in "${START_DIR}"/*/; do
     #echo $STUDENT
     #ls "${STUDENT}Submission attachment(s)"/*.lean
     for LEANFILE in "${STUDENT}Submission attachment(s)"/*.lean;
     do
         rm -f "${STUDENT}Submission attachment(s)"/*.olean
-        #VALID=$(lean --make "$LEANFILE"|echo $?)
-        #echo $VALID
-        lean --make "$LEANFILE" > /dev/null
+
+        lean --make "${LEANFILE}" > /dev/null
+
         VALID=$(echo $?)
-        if [ "$VALID" -eq 0 ]; then
-            echo "Type checked $student/'Submission attachment(s)'/$LEANFILE successfully!"
+
+        if [ "${VALID}" -eq 0 ]; then
+            echo "Type checked ${LEANFILE} successfully!"
         else
-            echo "FAILED TYPECHECK: $student/'Submission attachment(s)'/$LEANFILE"
+            echo "FAILED TYPECHECK: ${LEANFILE}"
         fi
         
-        echo $LEANFILE
+        #echo ${LEANFILE}
     done
 done
