608a5e4c858241b9a65ad7788b6cb4397b72a328
William Zhang <willzhang05@gmail.com>
2019-02-20 12:05:41 -0500

    fix feh bg

--- shell/connect_display.sh
+++ shell/connect_display.sh
@@ -7,4 +7,4 @@ if [ $1 == "off" ]; then
 fi
 
 xrandr --output eDP1 --auto --pos 0x0 --output DP1-1 --crtc 1 --primary --auto --pos 1920x0 --output DP1-2-1 --crtc 2 --auto --pos 3840x0
-feh --bg-fill ~/Pictures/background.jpg
+feh --bg-fill --no-xinerama ~/Pictures/odaiba.jpg
