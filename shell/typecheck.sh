98f658d00edb2487a7330cdafeae0917373600a3
William Zhang <willzhang05@gmail.com>
2019-02-20 19:32:00 -0500

    check for sorry

--- shell/typecheck.sh
+++ shell/typecheck.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-## This script is used to automatically typecheck student-submitted lean files.
-## It takes a path to the student subdirectories.
 
 if [ $# -ne 1 ]; then
     echo "Please provide a path to student submissions!"
@@ -14,17 +12,27 @@ for STUDENT in "${START_DIR}"/*/; do
     #ls "${STUDENT}Submission attachment(s)"/*.lean
     for LEANFILE in "${STUDENT}Submission attachment(s)"/*.lean;
     do
+        echo ""
         rm -f "${STUDENT}Submission attachment(s)"/*.olean
 
         lean --make "${LEANFILE}" > /dev/null
 
         VALID=$(echo $?)
+        SORRY=$(cat "${LEANFILE}" | grep "sorry")
+        
+        if [[ ! -z "${SORRY}" ]]; then
+            echo "Incomplete definition found:"
+            echo ""
+            cat "${LEANFILE}" | grep -ni -B 4 -A 2 "sorry"
+            echo ""
+        fi
 
-        if [ "${VALID}" -eq 0 ]; then
+        if [[ "${VALID}" -eq 0 && -z "${SORRY}" ]]; then
             echo "Type checked ${LEANFILE} successfully!"
         else
             echo "FAILED TYPECHECK: ${LEANFILE}"
         fi
+        
         
         #echo ${LEANFILE}
     done
